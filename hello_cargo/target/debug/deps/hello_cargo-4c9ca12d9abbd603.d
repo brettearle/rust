f135b3554e7ee45f9901804bbe89c1a6be496247
Brett Earle <100062845+brettearle@users.noreply.github.com>
2022-08-01 06:35:14 +0930

    Delete hello_cargo/target directory

--- hello_cargo/target/debug/deps/hello_cargo-4c9ca12d9abbd603.d
+++
@@ -1,5 +0,0 @@
-/home/brett/rust/hello_cargo/target/debug/deps/hello_cargo-4c9ca12d9abbd603.rmeta: src/main.rs
-
-/home/brett/rust/hello_cargo/target/debug/deps/hello_cargo-4c9ca12d9abbd603.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/brett/rust/hello_cargo/target/debug/hello_cargo: /home/brett/rust/hello_cargo/src/main.rs
